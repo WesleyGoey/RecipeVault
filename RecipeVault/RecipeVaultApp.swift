22e0ce7eedf214fc9fad98831a648b1bfccd1ee8
WesleyGoey <wesley.goey@gmail.com>
2026-05-13 22:14:29 +0700

    p

---
+++ RecipeVault/RecipeVaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeVaultApp.swift
+//  RecipeVault
+//
+//  Created by Wesley Goey on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct RecipeVaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
