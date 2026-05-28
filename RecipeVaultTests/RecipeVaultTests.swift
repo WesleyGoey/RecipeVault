3fcf61b7abc29d11dd2f4311dde0a3678a2a9c90
Sean Lawton <slawton01@student.ciputra.ac.id>
2026-05-28 22:14:44 +0700

    Adding the test file template only

---
+++ RecipeVaultTests/RecipeVaultTests.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeVaultTests.swift
+//  RecipeVaultTests
+//
+//  Created by Sean tandjaja on 28/05/26.
+//
+
+import Testing
+
+struct RecipeVaultTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
